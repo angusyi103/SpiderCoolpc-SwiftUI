f6d4ee1003f689b3bd9d215e8c8269dabe5bc00f
Angus Yi <angusyi103@gmail.com>
2021-09-01 17:10:19 +0800

    cell ui & gitignore

--- SpiderCoolpc/SpiderPage.swift
+++ SpiderCoolpc/SpiderPage.swift
@@ -11,17 +11,18 @@ struct SpiderPage: View {
     
     @ObservedObject var gpuDataHandler = GPUDataHandler()
     
+    init() {
+        gpuDataHandler.getCoolpcGPUDatas()
+    }
+    
     var body: some View {
         Button("Refresh", action: {
             gpuDataHandler.getCoolpcGPUDatas()
-        }).onAppear {
-            gpuDataHandler.getCoolpcGPUDatas()
-        }
+        })
         
         List {
-            let headerGpuInfo = GPUInfo(model: "型號", brand: "廠牌", MHz: "MHz", length: "長度", warranty: "保固", price: "價錢")
             ScrollView(.horizontal, showsIndicators: true) {
-                Section(header: GPUListCell(gpu: headerGpuInfo)) {
+                Section(header: GPUHeaderView()) {
                     ForEach(gpuDataHandler.gpuInfoAry) { gpu in
                         GPUListCell(gpu: gpu)
                             .frame(height: 20, alignment: .center)
@@ -30,6 +31,9 @@ struct SpiderPage: View {
             }
         }
         .environment(\.defaultMinListRowHeight, 20)
+        .onAppear {
+            gpuDataHandler.getCoolpcGPUDatas()
+        }
     }
 }
 
@@ -41,38 +45,109 @@ struct SpiderPage_Previews: PreviewProvider {
 }
 #endif
 
+struct GPUHeaderView: View {
+    let gpuInfoStringMaxLength = GpuInfoStringMaxLength.shared
+    var body: some View {
+        HStack {
+            HStack {
+                setupHeaderText(str: "廠牌")
+                setupFilterButton()
+                Divider()
+            }.frame(width: CGFloat(gpuInfoStringMaxLength.brand ?? 0) + 10, height: nil, alignment: .center)
+            HStack {
+                setupHeaderText(str: "型號")
+                setupFilterButton()
+                Divider()
+            }.frame(width: CGFloat(gpuInfoStringMaxLength.model ?? 0) + 10, height: nil, alignment: .center)
+            HStack {
+                setupHeaderText(str: "MHz")
+                setupFilterButton()
+                Divider()
+            }.frame(width: CGFloat(gpuInfoStringMaxLength.MHz ?? 0) + 10, height: nil, alignment: .center)
+            HStack {
+                setupHeaderText(str: "長度")
+                setupFilterButton()
+                Divider()
+            }.frame(width: CGFloat(gpuInfoStringMaxLength.length ?? 0) + 10, height: nil, alignment: .center)
+            HStack {
+                setupHeaderText(str: "保固")
+                setupFilterButton()
+                Divider()
+            }.frame(width: CGFloat(gpuInfoStringMaxLength.warranty ?? 0) + 10, height: nil, alignment: .center)
+            HStack {
+                setupHeaderText(str: "價錢")
+                setupFilterButton()
+                Divider()
+            }.frame(width: CGFloat(gpuInfoStringMaxLength.price ?? 0) + 10, height: nil, alignment: .center)
+            Spacer()
+        }
+    }
+    
+    private func setupHeaderText(str: String) -> some View {
+        return Text(str)
+            .font(.system(size: 17))
+            .fontWeight(.semibold)
+//              .frame(width: CGFloat(gpuInfoStringMaxLength.brand ?? 0), height: nil, alignment: .center)
+            .foregroundColor(.green)
+            .background(Color.yellow)
+    }
+    
+    private func setupFilterButton() -> some View {
+        return
+            Button(action: {
+                print("button pressed")
+            }, label: {
+                Image("down_arrow")
+                    .resizable()
+                    .frame(width: 10, height: 10, alignment: .center)
+            })
+    }
+}
+
 struct GPUListCell: View {
     let gpu: GPUInfo
     let gpuInfoStringMaxLength = GpuInfoStringMaxLength.shared
     var body: some View {
         HStack {
             Group {
-                Text(gpu.brand ?? "").frame(width: CGFloat(gpuInfoStringMaxLength.brand ?? 0) + 10, height: nil, alignment: .center)
+                Text(gpu.brand ?? "")
+                    .font(.system(size: 15))
+                    .frame(width: CGFloat(gpuInfoStringMaxLength.brand ?? 0), height: nil, alignment: .center)
                     .foregroundColor(gpu.available ?? true ? .green : .red)
                 Divider()
             }
             Group {
-                Text(gpu.model ?? "").frame(width: CGFloat(gpuInfoStringMaxLength.model ?? 0) + 10, height: nil, alignment: .center)
+                Text(gpu.model ?? "")
+                    .font(.system(size: 15))
+                    .frame(width: CGFloat(gpuInfoStringMaxLength.model ?? 0) + 10, height: nil, alignment: .center)
                     .foregroundColor(gpu.available ?? true ? .green : .red)
                 Divider()
             }
             Group {
-                Text(gpu.MHz ?? "").frame(width: CGFloat(gpuInfoStringMaxLength.MHz ?? 0) + 10, height: nil, alignment: .center)
+                Text(gpu.MHz ?? "")
+                    .font(.system(size: 15))
+                    .frame(width: CGFloat(gpuInfoStringMaxLength.MHz ?? 0) + 10, height: nil, alignment: .center)
                     .foregroundColor(gpu.available ?? true ? .green : .red)
                 Divider()
             }
             Group {
-                Text(gpu.length ?? "").frame(width: CGFloat(gpuInfoStringMaxLength.length ?? 0) + 10, height: nil, alignment: .center)
+                Text(gpu.length ?? "")
+                    .font(.system(size: 15))
+                    .frame(width: CGFloat(gpuInfoStringMaxLength.length ?? 0) + 10, height: nil, alignment: .center)
                     .foregroundColor(gpu.available ?? true ? .green : .red)
                 Divider()
             }
             Group {
-                Text(gpu.warranty ?? "").frame(width: CGFloat(gpuInfoStringMaxLength.warranty ?? 0) + 10, height: nil, alignment: .center)
+                Text(gpu.warranty ?? "")
+                    .font(.system(size: 15))
+                    .frame(width: CGFloat(gpuInfoStringMaxLength.warranty ?? 0) + 10, height: nil, alignment: .center)
                     .foregroundColor(gpu.available ?? true ? .green : .red)
                 Divider()
             }
             Group {
-                Text(gpu.price ?? "").frame(width: CGFloat(gpuInfoStringMaxLength.price ?? 0) + 10, height: nil, alignment: .center)
+                Text(gpu.price ?? "")
+                    .font(.system(size: 15))
+                    .frame(width: CGFloat(gpuInfoStringMaxLength.price ?? 0) + 10, height: nil, alignment: .center)
                     .foregroundColor(gpu.available ?? true ? .green : .red)
                 Divider()
             }
